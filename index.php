8dd3fc60e47ff17e1489d296f622d34ac14ffee0
Marsmalow20 <davidrenaldy1@gmail.com>
2021-04-20 00:53:14 +0700

    fix login page on index.php

--- index.php
+++ index.php
@@ -39,13 +39,13 @@
                 </div>
                 <label for="login-as">Login As :</label>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                    <input class="form-check-input" type="radio" name="login-as" id="flexRadioDefault1" checked>
                     <label class="form-check-label" for="flexRadioDefault1">
                         Mahasiswa
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                    <input class="form-check-input" type="radio" name="login-as" id="flexRadioDefault2">
                     <label class="form-check-label" for="flexRadioDefault2">
                         Dosen
                     </label>
